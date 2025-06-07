bc51ac469144fede5c8ddec45b0a1fa08d42b901
Mahedul-Karim <karimrupo21@gmail.com>
2025-06-07 15:54:37 +0600

    user queries added

--- src/pages/queries/MyQueries.jsx
+++ src/pages/queries/MyQueries.jsx
@@ -3,30 +3,47 @@ import Title from "@/components/home/sections/Title";
 import GridCard from "@/components/queries/GridCard";
 import Banner from "@/components/queries/my-queries/Banner";
 import EmptyQuery from "@/components/queries/my-queries/EmptyQuery";
+import { useApi } from "@/hooks/useApi";
 import { dummyQueries } from "@/lib/data";
-import React from "react";
+import { Loader } from "lucide-react";
+import React, { useEffect, useState } from "react";
 
 const MyQueries = () => {
+  const [data, setData] = useState([]);
+
+  const { isLoading, getQuery } = useApi();
+
+  useEffect(() => {
+    getQuery({
+      url: "query/my-query",
+    }).then((res) => setData(res.queries));
+  }, []);
+
   return (
     <main className="border-t border-border">
       <Container className="py-8 md:py-16">
         <Banner />
         <div className="mt-8">
           <Title>My Queries</Title>
-          {dummyQueries?.length === 0 ? (
-            <EmptyQuery />
-          ) : (
+          {isLoading && (
+            <div className="h-50 grid place-items-center">
+              <Loader className="text-primary size-12 animate-spin" />
+            </div>
+          )}
+          {!isLoading && data?.length === 0 && <EmptyQuery />}
+
+          {!isLoading && data?.length > 0 && (
             <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xs:gap-4 gap-2">
-              {dummyQueries.map((data, i) => (
+              {data?.map((query) => (
                 <GridCard
-                  key={i}
-                  id={i}
-                  productName={data.productName}
-                  productBrand={data.productBrand}
-                  productImageURL={data.productImageURL}
-                  queryTitle={data.queryTitle}
-                  boycottingReason={data.boycottingReason}
-                  recommendationCount={data.recommendationCount}
+                  key={query._id}
+                  id={query._id}
+                  productName={query.productName}
+                  productBrand={query.productBrand}
+                  productImageURL={query.productImage}
+                  queryTitle={query.queryTitle}
+                  boycottingReason={query.boycottingReason}
+                  recommendationCount={query.recommendationCount}
                   isUserQuery
                 />
               ))}
